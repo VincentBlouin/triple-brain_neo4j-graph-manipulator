91071327b1af3f5aa3c2e2946265f419d3a604e5
Vincent Blouin <vincent.blouin@gmail.com>
2014-12-15 07:22:56 -0500

    accept suggestion performance

--- src/main/java/org/triple_brain/module/neo4j_graph_manipulator/graph/graph/vertex/Neo4jVertexInSubGraphOperator.java
+++ src/main/java/org/triple_brain/module/neo4j_graph_manipulator/graph/graph/vertex/Neo4jVertexInSubGraphOperator.java
@@ -272,34 +272,38 @@ public class Neo4jVertexInSubGraphOperator implements VertexInSubGraphOperator, 
     }
 
     @Override
-    public EdgeOperator acceptSuggestion(SuggestionPojo suggestion) {
-        //todo the method operations should somehow be in a batch execution
-        EdgeOperator newEdge = addVertexAndRelation();
-        newEdge.label(suggestion.label());
-        VertexOperator newVertex = newEdge.destinationVertex();
-        if(suggestion.getSameAs() != null){
-            newEdge.addSameAs(
-                    new IdentificationPojo(
-                            suggestion.getSameAs().uri(),
-                            suggestion.getSameAs()
-                    )
-            );
-            newVertex.addType(
-                    new IdentificationPojo(
-                            suggestion.getSameAs().uri(),
-                            suggestion.getSameAs()
-                    )
-            );
-        }
-        if(suggestion.getType() != null){
-            newVertex.addType(
-                    new IdentificationPojo(
-                            suggestion.getType().uri(),
-                            suggestion.getType()
-                    )
-            );
-        }
-        return newEdge;
+    public EdgeOperator acceptSuggestion(final SuggestionPojo suggestion) {
+        return restApi.executeBatch(new BatchCallback<EdgeOperator>() {
+            @Override
+            public EdgeOperator recordBatch(RestAPI batchRestApi) {
+                EdgeOperator newEdge = addVertexAndRelation();
+                newEdge.label(suggestion.label());
+                VertexOperator newVertex = newEdge.destinationVertex();
+                if(suggestion.getSameAs() != null){
+                    newEdge.addSameAs(
+                            new IdentificationPojo(
+                                    suggestion.getSameAs().uri(),
+                                    suggestion.getSameAs()
+                            )
+                    );
+                    newVertex.addType(
+                            new IdentificationPojo(
+                                    suggestion.getSameAs().uri(),
+                                    suggestion.getSameAs()
+                            )
+                    );
+                }
+                if(suggestion.getType() != null){
+                    newVertex.addType(
+                            new IdentificationPojo(
+                                    suggestion.getType().uri(),
+                                    suggestion.getType()
+                            )
+                    );
+                }
+                return newEdge;
+            }
+        });
     }
 
     @Override
