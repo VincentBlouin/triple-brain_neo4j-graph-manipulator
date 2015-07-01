fcefd2cb4b791d0164591ba75ead40b346e82b1a
Vincent Blouin <vincent.blouin@gmail.com>
2015-07-01 19:57:51 -0400

    removes_existing_related_identifications_before_rebuild

--- src/main/java/guru/bubl/module/neo4j_graph_manipulator/graph/graph/Neo4jWholeGraph.java
+++ src/main/java/guru/bubl/module/neo4j_graph_manipulator/graph/graph/Neo4jWholeGraph.java
@@ -4,6 +4,10 @@
 
 package guru.bubl.module.neo4j_graph_manipulator.graph.graph;
 
+import guru.bubl.module.model.graph.GraphElement;
+import guru.bubl.module.model.graph.GraphElementOperator;
+import guru.bubl.module.model.graph.GraphElementOperatorFactory;
+import org.apache.commons.lang.StringUtils;
 import org.neo4j.graphdb.Node;
 import org.neo4j.rest.graphdb.query.QueryEngine;
 import org.neo4j.rest.graphdb.util.QueryResult;
@@ -36,6 +40,9 @@ public class Neo4jWholeGraph implements WholeGraph {
 
     @Inject
     protected SchemaFactory schemaFactory;
+
+    @Inject
+    protected GraphElementOperatorFactory graphElementFactory;
 
     @Override
     public Iterator<VertexInSubGraphOperator> getAllVertices() {
@@ -120,6 +127,41 @@ public class Neo4jWholeGraph implements WholeGraph {
                         iterator.next().get("uri").toString()
                 );
                 return schemaFactory.withUri(
+                        uri
+                );
+            }
+
+            @Override
+            public void remove() {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
+    }
+
+    @Override
+    public Iterator<GraphElementOperator> getAllGraphElements(){
+        return new Iterator<GraphElementOperator>() {
+            QueryResult<Map<String, Object>> result = queryEngine.query(
+                    "START n=node:node_auto_index('" +
+                            "( " + Neo4jFriendlyResource.props.type + ":" +
+                            StringUtils.join(GraphElementType.names(), " OR type:") +
+                            ") " +
+                            "') " +
+                            "RETURN n." + Neo4jFriendlyResource.props.uri + " as uri",
+                    Collections.emptyMap()
+            );
+            Iterator<Map<String, Object>> iterator = result.iterator();
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public GraphElementOperator next() {
+                URI uri = URI.create(
+                        iterator.next().get("uri").toString()
+                );
+                return graphElementFactory.withUri(
                         uri
                 );
             }
